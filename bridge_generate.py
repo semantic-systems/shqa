d7b5bbb50c88b5ca63f622cf2b75df57c49881ce
Tilahun Abedissa <58974800+huntila@users.noreply.github.com>
2025-01-13 16:33:15 +0100

    add bridge generate.py file

---
+++ bridge_generate.py
@@ -0,0 +1,34 @@
+import llms
+import utils
+
+def generate_assertion_statement(sub_question_phrase, entity, flag=False):
+    prompt = f"""[INST]Task: Form a sentence by joining the given PHRASE and ANSWER. Be sure the sentence is grammatically and semantically correct.
+            Only return the sentence.
+            [/INST]
+            PHRASE: {sub_question_phrase}
+            ANSWER:{entity}
+
+            sentence:
+    """
+    if flag:
+        prompt = f"""[INST]Task: Replace part of the QUESTION using the given ANSWER and form a sentence. Be sure the sentence is grammatically and semantically correct.
+            Only return the sentence.
+            QUESTION: What is the primary language of instruction at the institution where Yves Moreau is affiliated?
+            ANSWER: University of Leuven
+            sentence: The institution where Yves Moreau affiliated is University of Leuven.
+            QUESTION: When was the establishment year of the academic institution of Fang Liu?
+            ANSWER: Notre Dame University
+            sentence: The academic institution of Fang Liu is Notre Dame University.
+            [/INST]
+            QUESTION: {sub_question_phrase}
+            ANSWER:{entity}
+
+            sentence:
+    """
+    assertion_sentence = llms.chatgpt(prompt, 8)
+    utils.write_to_json([assertion_sentence], "chain_of_assertion/assertion_sentence.json")
+    assertion_sentence_formatted = utils.load_json_data("chain_of_assertion/assertion_sentence.json")
+    if 'text' in assertion_sentence_formatted[0]:
+        return assertion_sentence_formatted[0]['text']
+    else:
+        return ''
